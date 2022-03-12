72a7f649e60891fade9cde80e31e779c65e1de82
Brian <godonangaming@gmail.com>
2022-03-12 14:01:04 -0600

    Auto stash before merge of "main" and "origin/main"

---
+++ sculptor/generic.py
@@ -0,0 +1,463 @@
+# flake8: noqa
+"""
+Module which contains most imports and data unit tests
+might need, to reduce the amount of boilerplate.
+"""
+import os
+import sys
+import json
+import copy
+import time
+import shutil
+import timeit
+import base64
+import inspect
+import logging
+import platform
+import warnings
+import tempfile
+import unittest
+import threading
+import itertools
+import contextlib
+import subprocess
+import collections
+
+import trimesh
+
+from trimesh.base import Trimesh
+from trimesh.constants import tol, tol_path
+from collections import deque
+from copy import deepcopy
+
+
+from distutils.spawn import find_executable
+
+try:
+    # Python 3
+    from http.server import SimpleHTTPRequestHandler
+    import socketserver
+except ImportError:
+    # Python 2
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+    import SocketServer as socketserver
+
+import numpy as np
+
+# should we require all soft dependencies
+# this is set in the docker images to catch missing packages
+all_dep = 'alldep' in ''.join(sys.argv)
+
+if all_dep:
+    # make sure pyembree is importable
+    from pyembree import rtcore_scene
+
+try:
+    import sympy as sp
+except ImportError as E:
+    if all_dep:
+        raise E
+    sp = None
+
+# make sure functions know they should run additional
+# potentially slow validation checks and raise exceptions
+trimesh.util._STRICT = True
+trimesh.constants.tol.strict = True
+trimesh.constants.tol_path.strict = True
+
+
+try:
+    from shapely.geometry import Point, Polygon, LineString
+    has_path = True
+except ImportError as E:
+    if all_dep:
+        raise E
+    has_path = False
+
+try:
+    from scipy import spatial, sparse
+except BaseException as E:
+    if all_dep:
+        raise E
+
+try:
+    import networkx as nx
+except BaseException as E:
+    pass
+
+# find_executable for binvox
+    has_binvox = trimesh.exchange.binvox.binvox_encoder is not None
+
+# Python version as a tuple, i.e. [3, 6]
+PY_VER = (sys.version_info.major,
+          sys.version_info.minor)
+
+# some repeatable homogeneous transforms to use in tests
+transforms = [trimesh.transformations.euler_matrix(np.pi / 4, i, 0)
+              for i in np.linspace(0.0, np.pi * 2.0, 100)]
+# should be a (100, 4, 4) float
+transforms = np.array(transforms)
+
+try:
+    # do the imports for Python 2
+    from cStringIO import StringIO
+    PY3 = False
+except ImportError:
+    # if that didn't work we're probably on Python 3
+    from io import StringIO
+    from io import BytesIO
+    PY3 = True
+
+# are we on linux
+is_linux = 'linux' in platform.system().lower()
+
+# find the current absolute path using inspect
+dir_current = os.path.dirname(
+    os.path.abspath(os.path.expanduser(__file__)))
+# the absolute path for our reference models
+dir_models = os.path.abspath(
+    os.path.join(dir_current, '..', 'models'))
+# the absolute path for our 2D reference models
+dir_2D = os.path.abspath(
+    os.path.join(dir_current, '..', 'models', '2D'))
+# the absolute path for our test data and truth
+dir_data = os.path.abspath(
+    os.path.join(dir_current, 'data'))
+
+# a logger for tests to call
+log = logging.getLogger('trimesh')
+log.addHandler(logging.NullHandler())
+
+# turn strings / bytes into file- like objects
+io_wrap = trimesh.util.wrap_as_stream
+
+
+def random(*args, **kwargs):
+    """
+    A random function always seeded from the same value.
+
+    Replaces: np.random.random(*args, **kwargs)
+    """
+    state = np.random.RandomState(seed=1)
+    return state.random_sample(*args, **kwargs)
+
+
+def _load_data():
+    """
+    Load the JSON files from our truth directory.
+    """
+    data = {}
+    for file_name in os.listdir(dir_data):
+        name, extension = os.path.splitext(file_name)
+        if extension != '.json':
+            continue
+        file_path = os.path.join(dir_data, file_name)
+        with open(file_path, 'r') as file_obj:
+            data[name] = json.load(file_obj)
+
+    data['model_paths'] = [os.path.join(dir_models, f)
+                           for f in os.listdir(dir_models)]
+    data['2D_files'] = [os.path.join(dir_2D, f) for f in os.listdir(dir_2D)]
+    return data
+
+
+def get_mesh(file_name, *args, **kwargs):
+    """
+    Get a mesh from the models directory by name.
+
+    Parameters
+    -------------
+    file_name : str
+      Name of model in /models/
+    *args : [str]
+      Additional files to load
+
+    Returns
+    -----------
+    meshes : trimesh.Trimesh or list
+      Single mesh or list of meshes from args
+    """
+    meshes = collections.deque()
+    for name in np.append(file_name, args):
+        location = get_path(name)
+        log.info('loading mesh from: %s', location)
+        meshes.append(trimesh.load(location, **kwargs))
+    if len(meshes) == 1:
+        return meshes[0]
+    return list(meshes)
+
+
+def get_path(file_name):
+    """
+    Get the absolute location of a referenced model file.
+
+    Parameters
+    ------------
+    file_name : str
+      Name of model relative to `repo/models`
+
+    Returns
+    ------------
+    full : str
+      Full absolute path to model.
+    """
+    return os.path.abspath(
+        os.path.join(dir_models, file_name))
+
+
+@contextlib.contextmanager
+def serve_meshes():
+    """
+    This context manager serves meshes over HTTP at some
+    available port.
+    """
+    class _ServerThread(threading.Thread):
+        def run(self):
+            os.chdir(dir_models)
+            Handler = SimpleHTTPRequestHandler
+            self.httpd = socketserver.TCPServer(('', 0), Handler)
+            _, self.port = self.httpd.server_address
+            self.httpd.serve_forever()
+
+    t = _ServerThread()
+    t.daemon = False
+    t.start()
+    time.sleep(0.2)
+    yield 'http://localhost:{}'.format(t.port)
+    t.httpd.shutdown()
+    t.join()
+
+
+def get_meshes(count=np.inf,
+               raise_error=False,
+               only_watertight=True):
+    """
+    Get meshes to test with.
+
+    Parameters
+    ----------
+    count : int
+      Approximate number of meshes you want
+    raise_error : bool
+      If True raise a ValueError if a mesh
+      that should be loadable returns a non- Trimesh object.
+
+    Returns
+    ----------
+    meshes : list
+      Trimesh objects from models folder
+    """
+    # use deterministic file name order
+    file_names = sorted(os.listdir(dir_models))
+
+    meshes = []
+    for file_name in file_names:
+        extension = trimesh.util.split_extension(file_name).lower()
+        if extension in trimesh.available_formats():
+            loaded = trimesh.util.make_sequence(get_mesh(file_name))
+            for m in loaded:
+                # is the loaded mesh a Geometry object or a subclass:
+                # Trimesh, PointCloud, Scene
+                type_ok = isinstance(m, trimesh.parent.Geometry)
+                if raise_error and not type_ok:
+                    raise ValueError('%s returned a non- Trimesh object!',
+                                     file_name)
+                if not isinstance(m, trimesh.Trimesh) or (
+                        only_watertight and not m.is_watertight):
+                    continue
+                meshes.append(m)
+                yield m
+        else:
+            log.warning('%s has no loader, not running test on!',
+                        file_name)
+
+        if len(meshes) >= count:
+            break
+
+
+def get_2D(count=None):
+    """
+    Get Path2D objects to test with.
+
+    Parameters
+    --------------
+    count : int
+      Number of 2D drawings to return
+
+    Yields
+    --------------
+    path : trimesh.path.Path2D
+      Drawing from models folder
+    """
+    # if no path loading return empty list
+    if not has_path:
+        raise StopIteration
+
+    # all files in the 2D models directory
+    listdir = sorted(os.listdir(dir_2D))
+    # if count isn't passed return all files
+    if count is None:
+        count = len(listdir)
+    # save resulting loaded paths
+    paths = []
+    for file_name in listdir:
+        # check to see if the file is loadable
+        ext = trimesh.util.split_extension(file_name)
+        if ext not in trimesh.available_formats():
+            continue
+        # full path
+        location = os.path.join(dir_2D, file_name)
+        try:
+            paths.append(trimesh.load(location))
+        except BaseException as E:
+            log.error('failed on: {}'.format(file_name),
+                      exc_info=True)
+            raise E
+
+        yield paths[-1]
+
+        # if we don't need every path break
+        if len(paths) >= count:
+            break
+
+
+def check_path2D(path):
+    """
+    Make basic assertions on Path2D objects
+    """
+    # root count should be the same as the closed polygons
+    assert len(path.root) == len(path.polygons_full)
+
+    # make sure polygons are really polygons
+    assert all(type(i).__name__ == 'Polygon'
+               for i in path.polygons_full)
+    assert all(type(i).__name__ == 'Polygon'
+               for i in path.polygons_closed)
+
+    # these should all correspond to each other
+    assert len(path.discrete) == len(path.polygons_closed)
+    assert len(path.discrete) == len(path.paths)
+
+    # make sure None polygons are not referenced in graph
+    assert all(path.polygons_closed[i] is not None
+               for i in path.enclosure_directed.nodes())
+
+    if any(e.color is not None for e in path.entities):
+        assert path.colors.shape == (len(path.entities), 4)
+
+
+def scene_equal(a, b):
+    """
+    Do a simple check on two scenes and assert
+    that they have the same geometry.
+
+    Parameters
+    ------------
+    a : trimesh.Scene
+      Object to be compared
+    b : trimesh.Scene
+      Object to be compared
+    """
+    # should have the same number of geometries
+    assert len(a.geometry) == len(b.geometry)
+    for k, m in a.geometry.items():
+        # each mesh should correspond by name
+        # and have the same volume
+        assert np.isclose(
+            m.volume, b.geometry[k].volume, rtol=0.001)
+
+
+def texture_equal(a, b):
+    """
+    Make sure the texture in two meshes have the same
+    face-uv-position results.
+
+    Parameters
+    ------------
+    a : trimesh.Trimesh
+      Mesh to check
+    b : trimesh.Trimesh
+      Should be identical
+    """
+    try:
+        from scipy.spatial import cKDTree
+    except BaseException:
+        log.error('no scipy for check!', exc_info=True)
+        return
+
+    # an ordered position-face-UV blob to check
+    pa = np.hstack((a.vertices, a.visual.uv))[
+        a.faces].reshape((-1, 15))
+    pb = np.hstack((b.vertices, b.visual.uv))[
+        b.faces].reshape((-1, 15))
+    # query their actual ordered values against each other
+    q = cKDTree(pa).query_ball_tree(cKDTree(pb), r=1e-4)
+    assert all(i in match for i, match in enumerate(q))
+
+
+def check_fuze(fuze):
+    """
+    Check the classic textured mesh: a fuze bottle
+    """
+    # these loaded fuze bottles should have textures
+    assert isinstance(
+        fuze.visual, trimesh.visual.TextureVisuals)
+    # image should be loaded with correct resolution
+    assert fuze.visual.material.image.size == (1024, 1024)
+    # UV coordinates should be unmerged correctly
+    assert len(fuze.visual.uv) == 664
+    # UV coordinates shouldn't be all zero- ish
+    assert fuze.visual.uv[:, :2].ptp(axis=0).min() > 0.1
+    # UV coordinates should be between zero and 1
+    assert fuze.visual.uv.min() > -tol.merge
+    assert fuze.visual.uv.max() < 1 + tol.merge
+    # check color factors
+    factors = [fuze.visual.material.ambient,
+               fuze.visual.material.diffuse,
+               fuze.visual.material.specular]
+    for f in factors:
+        # should be RGBA
+        assert len(f) == 4
+        assert f.dtype == np.uint8
+        # should be between 0 and 255
+        assert f.min() >= 0
+        assert f.max() <= 255
+        # should have some nonzero values
+        assert (f > 0).any()
+
+    # vertices should correspond with UV
+    assert fuze.vertices.shape == (664, 3)
+    # convert TextureVisuals to ColorVisuals
+    viz = fuze.visual.to_color()
+    assert viz.kind == 'vertex'
+    # should be actual colors defined
+    assert viz.vertex_colors.ptp(axis=0).ptp() != 0
+    # shouldn't crash
+    fuze.visual.copy()
+    fuze.visual.material.copy()
+
+
+def wrapload(exported, file_type, **kwargs):
+    """
+    Reload an exported byte blob into a mesh.
+
+    Parameters
+    -----------
+    exported : bytes or str
+      Exported mesh
+    file_type : str
+      Type of file to load
+    kwargs : dict
+      Passed to trimesh.load
+
+    Returns
+    ---------
+    loaded : trimesh.Trimesh
+      Loaded result
+    """
+    return trimesh.load(file_obj=trimesh.util.wrap_as_stream(exported),
+                        file_type=file_type,
+                        **kwargs)
+
+
+TemporaryDirectory = trimesh.util.TemporaryDirectory
--- sculptor/sculpt.py
+++ sculptor/sculpt.py
@@ -7,11 +7,38 @@ from PIL import Image
 from pathlib import Path
 from pycocotools.coco import COCO
 import cv2
+from tqdm import tqdm
+try:
+    from . import generic as g
+except BaseException:
+    import generic as g
+
+import pyglet
 import matplotlib.pyplot as plt
 np.set_printoptions(suppress=True)
+from collections import Counter
 
 collection_path = ".\\model_collection\\banana"
 inference_out = ".\\model_collection\\banana_infer"
+
+palette = {
+    '0': np.array([255, 0, 0, 255]),
+    '2': np.array([0, 0, 255, 255])
+}
+
+
+def export_mesh_with_texture(mesh, path):
+    # export the mesh including data
+    export, texture = trimesh.exchange.obj.export_obj(
+        mesh, include_color=True, include_texture=True, return_texture=True)
+
+    obj_path = os.path.join(path, 'file_name.obj')
+    with open(obj_path, 'w') as f:
+        f.write(export)
+    for k, v in texture.items():
+        with open(os.path.join('', k), 'wb') as f:
+            f.write(v)
+    print("Saved to", obj_path)
 
 def _Map3DTo2D(p3d, projection_matrix, camera_pose, image_width, image_height):
     """
@@ -19,8 +46,19 @@ def _Map3DTo2D(p3d, projection_matrix, camera_pose, image_width, image_height):
     """
     view_matrix = np.linalg.inv(camera_pose)
     mvp = np.dot(projection_matrix, view_matrix)
-    print(mvp.shape)
     p0 = np.append(p3d, [1])
+    #print("projection_matrix")
+    #print(projection_matrix)
+    #print("camera_pose")
+    #print(camera_pose)
+    #print("view_matrix", view_matrix.shape)
+    #print("projection_matrix", projection_matrix.shape)
+
+    #print("p3d", p3d.shape)
+
+    #print("mvp", mvp.shape)
+    #print("p0", p0.shape)
+
     e0 = np.dot(mvp, p0)
     e0[:3] /= e0[3]
     pos_x = e0[0]
@@ -35,7 +73,7 @@ def _Map3DTo2D(p3d, projection_matrix, camera_pose, image_width, image_height):
 
 def map3dto2d(vertex_np, frame_json, dims):
     image_width, image_height = dims
-    p3d = np.array(frame_json["intrinsics"])
+    p3d = vertex_np
     projection_matrix = np.array(frame_json["projectionMatrix"])
     camera_pose = np.array(frame_json["cameraPoseARFrame"])
 
@@ -44,13 +82,13 @@ def map3dto2d(vertex_np, frame_json, dims):
     camera_pose = np.reshape(camera_pose, (4, 4))
 
     #print(p3d)
-    #print(projection_matrix)
+    #print(projection_matrix.shape)
     #print(camera_pose)
 
     return _Map3DTo2D(p3d, projection_matrix, camera_pose, image_width, image_height)
  
         
-def get_valid_frames(vertex, frame_dict):
+def get_valid_frames(vertex_np, frame_dict):
     """
     Input vertex as a size (3,) numpy array
     Input frame_dict as a dictionary mapping frame json filepath and the image filepath
@@ -59,22 +97,18 @@ def get_valid_frames(vertex, frame_dict):
     List[0] will store the frame image dimensions
     List[1] will store the image stem name (no extension)
     """
-    valid_frame_list = [None] * 2
-    valid_frame_list[0] = []
-    valid_frame_list[1] = []
+    valid_frame_list = [[], []]
     for frame_json_pth in frame_dict.keys():
-        print(frame_json_pth)
         json_f = open(frame_json_pth)
         frame_json = json.load(json_f)
+
         frame_img_pth = frame_dict[frame_json_pth]
         img_pil = Image.open(frame_img_pth)
         dims = img_pil.size
 
-        if map3dto2d(vertex, frame_json, dims) is not None:
-            
+        if map3dto2d(vertex_np, frame_json, dims) is not None:
             valid_frame_list[0].append(dims)
             valid_frame_list[1].append(Path(frame_json_pth).stem)
-
     return valid_frame_list
 
 def _temp_models(frame_img_pth):
@@ -88,7 +122,7 @@ def model_execute(frame_img_pths):
 
     frame_results = []
 
-    for i in range(10):
+    for i in tqdm(range(num)):
         frame_img_pth = frame_img_pths[i]
         img_pil = Image.open(frame_img_pth)
         img_np = np.asarray(img_pil)
@@ -100,7 +134,7 @@ def model_execute(frame_img_pths):
         for j in range(len(anns)):
             mask += coco.annToMask(anns[j])
         frame_results.append(mask)
-        cv2.imwrite(os.path.join(inference_out, os.path.basename(frame_img_pth)), mask)
+        cv2.imwrite(os.path.join(inference_out, Path(frame_img_pth).stem + '.png'), mask)
 
     # Execute model on the image
     # For now, just a place holder temp function
@@ -109,6 +143,14 @@ def model_execute(frame_img_pths):
 
 
 def main():
+    #im = Image.open("C:\\Users\\Godonan\\Documents\\_Computing\\Food-Detection\\sculptor\\model_collection\\banana\\textured_output.jpg")
+    #mesh_tex = trimesh.load("C:\\Users\\Godonan\\Documents\\_Computing\\Food-Detection\\sculptor\\model_collection\\banana\\textured_output.obj", process=False)
+    #tex = trimesh.visual.TextureVisuals(image=im)
+    #mesh_tex.visual.texture = tex
+    #mesh_tex.show()
+    #export_mesh_with_texture(mesh_tex, "./")
+
+
     frame_json_pths = sorted(glob.glob(os.path.join(collection_path, "frame*.json")))
     frame_img_pths = sorted(glob.glob(os.path.join(collection_path, "frame*.jpg")))
     model_pth = glob.glob(os.path.join(collection_path, "*export.obj"))[0]
@@ -136,29 +178,150 @@ def main():
 
 
     # Load the mesh
-    mesh = trimesh.load(model_pth)
-    # mesh.show()
 
-    # Looping through each vertex in the mesh
-    for vertex in mesh.vertices:
-        # Get the list of valid frames
-        valid_frame_list = get_valid_frames(vertex, frame_dict)
-        print(vertex)
-        print(valid_frame_list)
-"""
-        # Get total amount of valid frames
-        total = len(valid_frame_list[1])
-        votes = 0
-        for i in range(total):
-            # Get the dimensions and stem name
-            dims = valid_frame_list[0][i]
-            stemname = valid_frame_list[1][i]
+    mesh = trimesh.load(model_pth, process=False)
 
-            # Convert vertex to numpy
-            vertex_np = np.array(vertex)
-            coord = map3dto2d(vertex_np, framename_dict[stemname][1])
-            category = frame_results_dict[]
-"""
+    mesh_weight_1 = trimesh.load(model_pth, process=False)
+    mesh_weight_5 = trimesh.load(model_pth, process=False)
+    mesh_weight_10 = trimesh.load(model_pth, process=False)
+    mesh_weight_20 = trimesh.load(model_pth, process=False)
+    mesh_weight_40 = trimesh.load(model_pth, process=False)
+    #mesh.show()
+
+    #mesh2 = trimesh.load("C:\\Users\\Godonan\\Documents\\!! Computing\Datasets\\OBJs\\banana-11-55-10\\2021_11_12_22_55_37\\export.obj")
+    #mesh2.show()
+
+    # Make a list of categories indexed by mesh.vertices index, default -1 invalid value
+    vertex_cats = [-1] * len(mesh.vertices)
+
+    num_faces = len(mesh.faces)
+    face_mask = [False] * num_faces
+    # Loop through each face
+    for arridx in tqdm(range(num_faces)):
+        face = mesh.faces[arridx]
+        face_vertex_cats = []
+        for vertex_idx in face:
+            # Get vertex_np
+            vertex_np = np.array(mesh.vertices[vertex_idx])
+            #print(vertex_np)
+
+            # First check if np exists in vertex_cats. if so, use that determined category
+            check = vertex_cats[vertex_idx]
+            if check != -1:
+                face_vertex_cats.append(check)
+                continue
+
+            # Otherwise, compute category
+
+            # Get the list of valid frames
+            valid_frame_list = get_valid_frames(vertex_np, frame_dict)
+            #print(valid_frame_list)
+
+            # Get total amount of valid frames
+            num_valid_frames = len(valid_frame_list[0])
+            voting_list = {}
+
+            # Check if there are any valid frames
+            if num_valid_frames < 1:
+                continue
+
+            for i in range(num_valid_frames):
+                # Get the dimensions and stem name
+                dims = valid_frame_list[0][i]
+
+                stemname = valid_frame_list[1][i]
+                json_f = open(framename_dict[stemname][0])
+                frame_json = json.load(json_f)
+
+                # Convert vertex to numpy
+                coord = map3dto2d(vertex_np, frame_json, dims)
+                if coord is not None:
+                    # Make int as otherwise rounding may result in out of bounds
+                    # results range form 0 to 1439.
+                    coord = (int(coord[0]), int(coord[1]))
+
+                    frame_result = framename_dict[stemname][2]
+                    category_id = frame_result[coord[1], coord[0]]
+                    if str(category_id) in voting_list:
+                        voting_list[str(category_id)] += 1
+                    else:
+                        voting_list[str(category_id)] = 1
+                else:
+                    print("Crack slipthrough!")
+
+            # Increase weight of a detection against the background
+            voting_weight_5 = {}
+            for key in voting_list.keys():
+                if key != "0":
+                    voting_weight_5[key] = voting_list[key] * 5
+                else:
+                    voting_weight_5[key] = voting_list[key]
+
+
+            voting_weight_10 = {}
+            for key in voting_list.keys():
+                if key != "0":
+                    voting_weight_10[key] = voting_list[key] * 10
+                else:
+                    voting_weight_10[key] = voting_list[key]
+
+            voting_weight_20 = {}
+            for key in voting_list.keys():
+                if key != "0":
+                    voting_weight_20[key] = voting_list[key] * 20
+                else:
+                    voting_weight_20[key] = voting_list[key]
+
+
+            voting_weight_40 = {}
+            for key in voting_list.keys():
+                if key != "0":
+                    voting_weight_40[key] = voting_list[key] * 40
+                else:
+                    voting_weight_40[key] = voting_list[key]
+
+            cat = max(voting_list, key=voting_list.get)
+            vertex_cats[vertex_idx] = cat
+
+
+            new_vert_color_1 = palette[str(max(voting_list, key=voting_list.get))]
+            mesh_weight_1.visual.vertex_colors[vertex_idx] = new_vert_color_1
+
+            new_vert_color_5 = palette[str(max(voting_weight_5, key=voting_weight_5.get))]
+            mesh_weight_5.visual.vertex_colors[vertex_idx] = new_vert_color_5
+
+            new_vert_color_10 = palette[str(max(voting_weight_10, key=voting_weight_10.get))]
+            mesh_weight_10.visual.vertex_colors[vertex_idx] = new_vert_color_10
+
+            new_vert_color_20 = palette[str(max(voting_weight_20, key=voting_weight_20.get))]
+            mesh_weight_20.visual.vertex_colors[vertex_idx] = new_vert_color_20
+
+            new_vert_color_40 = palette[str(max(voting_weight_40, key=voting_weight_40.get))]
+            mesh_weight_40.visual.vertex_colors[vertex_idx] = new_vert_color_40
+
+        # Now check the majority category of the three vertices, if tie the lowest matters
+        #face_vertex_cats.sort()
+        #num = max(face_vertex_cats, key=Counter(face_vertex_cats).get)
+        #color = palette[str(num)]
+        #mesh.visual.vertex_colors[vertex_idx] = color
+    # We now have a bool masking for vertices
+    #export_mesh_with_texture(mesh_weight_1, './mesh1')
+    #export_mesh_with_texture(mesh_weight_5, './mesh5')
+    #export_mesh_with_texture(mesh_weight_10, './mesh10')
+    #export_mesh_with_texture(mesh_weight_20, './mesh20')
+    #export_mesh_with_texture(mesh_weight_40, './mesh40')
+    mesh_weight_1.show()
+    mesh_weight_5.show()
+    mesh_weight_10.show()
+    mesh_weight_20.show()
+    mesh_weight_40.show()
+
+    mesh_weight_1.write('./mesh1/mesh1.ply')
+    mesh_weight_5.write('./mesh5/mesh1.ply')
+    mesh_weight_10.write('./mesh10/mesh1.ply')
+    mesh_weight_20.write('./mesh20/mesh1.ply')
+    mesh_weight_40.write('./mesh40/mesh1.ply')
+
 
         
 
